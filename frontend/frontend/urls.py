75a305baec2239406295b33c00e3b208ecad066e
Lautaro Barba <lau_mb94@hotmail.com>
2022-11-18 14:01:12 -0300

    Se crearon archivos basicos para la app en frontend

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import frontend.app.views as appViews
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', appViews.Home.as_view(), name='index'),
 ]
