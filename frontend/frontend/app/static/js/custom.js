fab1d2cb964fac2e30fe1088295df17819171cb8
Lautaro Barba <lau_mb94@hotmail.com>
2022-11-18 15:02:00 -0300

    Se commitea para seguir en casa :D

--- frontend/frontend/app/static/js/custom.js
+++ frontend/frontend/app/static/js/custom.js
@@ -1,1 +1,10 @@
+// Notifico en consola que se cargo el documento
 console.log('Custom JS cargado');
+
+// Selecciono el boton para agregarle acciones
+const buscarBtn = document.getElementById('buscarBtn');
+
+// Busco los usuarios al presionar el boton
+buscarBtn.onclick(() => {
+	console.log('Buscando usuarios...')
+});
